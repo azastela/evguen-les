d9387e7dd81f27631a6e5bf7cbb0f3787db647f4
Andrii Zastela <andrii_zastela@yahoo.com>
2014-07-13 11:51:20 +0300

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+EvguenLes::Application.config.secret_token = '8af99ff2916a029c4d2940a61618b34c7bc2b8be0ee0cd61bec4e41d8340da832d52a8085e3e89464d566df27703712645552d1fb73c09b863d9c4ab6e63b6d9'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+EvguenLes::Application.config.session_store :cookie_store, key: '_evguen_les_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# EvguenLes::Application.config.session_store :active_record_store
